d05aa0e1f6029554ae7087df9dc5aa1c3637b7e5
Paulo Henrique Braga <paulo@someletras.com.br>
2020-07-14 23:04:10 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Calculator from "./main/Calculator.jsx";
+
+ReactDOM.render(
+          <div>
+
+          <h1>
+            Calculator
+          </h1>
+          <Calculator></Calculator>
+          </div>,
+          document.getElementById("root"));
